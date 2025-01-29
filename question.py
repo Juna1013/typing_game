f04af9240af951addda697df146a6d12619bf63b
Junya <160022550+Juna1013@users.noreply.github.com>
2025-01-29 21:15:48 +0900

    Add files via upload

---
+++ question.py
@@ -0,0 +1,10 @@
+QUESTION = ["first",
+            "second",
+            "third",
+            "forth",
+            "fifth", 
+            "sixth",
+            "seventh",
+            "eighth",
+            "ninth",
+            "tenth"]
